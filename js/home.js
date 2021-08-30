4794c4e674036ca9bf492d35b84416b429ce4e95
Alireza_6 <areza.mirzayi@gmail.com>
2021-08-30 18:44:12 +0430

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,7 @@
+function searchAppear() {
+    a = document.querySelector(".search").classList.add("search-appear");
+}
+
+function searchClose() {
+    a = document.querySelector(".search").classList.remove("search-appear");
+}
